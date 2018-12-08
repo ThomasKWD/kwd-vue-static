35c6c19d7113fba5cb7256177427f5fc2835565c
Thomas Kühne <thomas@kuehne-webdienste.de>
2018-12-08 22:25:53 +0100

    read reference list and text body from API

--- pages/referenzen/_id.vue
+++ pages/referenzen/_id.vue
@@ -1,27 +1,37 @@
+<!--
+- TODO: get already fetched data (if so) via payload
+-->
 <template>
 	<article class="reference-post">
 		<h1>{{title}}</h1>
-		<div class="post-body" v-html="referenceHtml"></div>
+		<div class="post-body" v-html="body"></div>
 	</article>
 </template>
 
 <script>
-// mock data
-var refData = [
-	{
-		id : 1,
-		title : 'Tolle Website 1 content view',
-		body : '<p>Alles <b>klar</b> soweit!</p>'
-	}
-];
+import axios from 'axios'
 
 export default {
+	// mounted ({params}) {
+	// 	axios
+	// 		// .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+	// 		.get('https://www.kuehne-webdienste.de/api/articles/'+params.id+'/content') // default language
+	//         .then(response => (reference = response.data))
+	// },
+	// data () {
+	// 	return {
+	// 		reference : null
+	// 	}
+	// }
+
 	async asyncData({params}) {
-		var postData = refData[params.id - 1];
-		return {
-			title : postData.title,
-			referenceHtml : postData.body + '-- param:'+params.id
-		}
-	},
+		console.log('trying to request: ');
+		console.log(`https://www.kuehne-webdienste.de/api/articles/${params.id}/1/content`)
+		// ! construct only working, if var is named 'data'
+		const { data } = await axios.get(`https://www.kuehne-webdienste.de/api/articles/${params.id}/1/content`)
+		// since there is no real marker (except checking the names of the root categories) we still need to hard code the id of entry point
+
+		return data; // ! data Object returned directly thus just include the fields e.g. {{title}}
+	}
 }
 </script>
--- pages/referenzen/index.vue
+++ pages/referenzen/index.vue
@@ -12,36 +12,40 @@ TODO: preload custum routes as described in tutorial and pass already fetched co
 		<nav>
 			<ul>
 				<li v-for="r in references" v-bind:key="r.id">
-				    <nuxt-link v-bind:to="'/referenzen/'+r.id">{{ r.title }}</nuxt-link>
+					<nuxt-link v-bind:to="'/referenzen/'+r.id">{{ r.title }}</nuxt-link>
 				  </li>
 			</ul>
-
-			<!-- <pre>{{blogs}}</pre> -->
 		</nav>
+		<p>{{references}}</p>
 	</main>
 </template>
 
 <script>
 import axios from 'axios'
-// mock data
-var mockData = [
-	{
-		id : 1,
-		title : 'Tolle Website 1 content view',
-		body : '<p>Alles <b>klar</b> soweit!</p>'
-	}
-];
 
 export default {
-	async asyncData({params}) {
-		// since there is no real marker (except checking the names of the root categories) we still need to hard code the id of entry point
-		let { refData } = await axios.get('https://www.kuehne-webdienste/api/articles/3/1')
-		console.log ('refData following...')
-		console.log ('refData following...')
+	mounted () {
+		axios
+			// .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+			.get('https://www.kuehne-webdienste.de/api/articles/3/1')
+	        .then(response => (this.references = response.data.sub_articles))
+	},
+	data () {
 		return {
-			// list contains subentries of article 3
-			references : refData.list
+			references : null
 		}
 	}
+	// TODO: find out why below code not working:
+	// async asyncData({params}) {
+	// 	const { refData } = await axios.get('https://www.kuehne-webdienste.de/api/articles/3/1')
+	// 	// since there is no real marker (except checking the names of the root categories) we still need to hard code the id of entry point
+	//
+	// 	console.log ('refData following...')
+	// 	console.log ('refData following...')
+	// 	return {
+	// 		// list contains subentries of article 3
+	// 		references : refData
+	// 	}
+	// }
 }
 </script>
